12793560ac9b77a25f9fc3892c9d8753a2bb42eb
A-Guldborg <aguh@itu.dk>
2022-04-12 12:33:08 +0200

    Javascript now load receipts from a list of objects

--- receipts.js
+++ receipts.js
@@ -1,17 +1,31 @@
 function loadReceipts() {
-    const receipts = document.getElementById("receiptList");
+    const receiptList = document.getElementById("receiptList");
     const origReceipt = document.getElementById("originalReceipt");
 
-    if (getReceipts().length === 0) {
-        receipts.innerHTML = "<p>Could not find any receipts</p>";
-    } else {
-        for (let i = 0; i < getReceipts().length; i++) {
+    const receipts = getReceipts();
 
+    if (receipts.length === 0) {
+        receiptList.innerHTML = "<p>Could not find any receipts</p>";
+    } else {
+        for (let i = 0; i < receipts.length; i++) {
+            let clone = origReceipt.cloneNode(true);
+            clone.removeAttribute("id");
+            clone.querySelector(".name").innerHTML = receipts[i].name;
+            clone.querySelector(".price").innerHTML = receipts[i].price + ",-";
+            receiptList.appendChild(clone);
         }
     }
 }
 
-
 function getReceipts() {
-    return [];
+    return [
+        {
+            price: 32,
+            name: "Latte"
+        },
+        {
+            price: 27,
+            name: "Macchiato"
+        }
+    ];
 }
