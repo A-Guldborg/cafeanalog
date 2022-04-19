08a5be108e4b3e3eab42350f0236c630ad538031
A-Guldborg <aguh@itu.dk>
2022-04-19 18:01:24 +0200

    Removes code duplication shop items Adds a deductMoney function Clicking on shop items now reduces balance

--- main.js
+++ main.js
@@ -16,6 +16,73 @@ function loadMenu() {
     loadBalance();
 }
 
+function loadShopItems() {
+    const shopList = document.getElementById("allcoffee");
+    const origReceipt = document.getElementById("originalCoffee");
+
+    const items = getShopItems();
+
+    if (items.length === 0) {
+        shopList.innerHTML = "<p>Could not find any purchasable items</p>";
+    } else {
+        for (let i = 0; i < items.length; i++) {
+            let clone = origReceipt.cloneNode(true);
+            clone.setAttribute("id", "coffee" + i);
+            clone.setAttribute("onclick", "buyItem('" + items[i].name + "', " + items[i].price + ")");
+            clone.querySelector(".name").innerHTML = items[i].name;
+            clone.querySelector(".price").innerHTML = items[i].price + ",-";
+            shopList.appendChild(clone);
+        }
+    }
+}
+
+function getShopItems() {
+    return [
+        {
+            price: 32,
+            name: "Latte"
+        },
+        {
+            price: 30,
+            name: "Cocoa"
+        },
+        {
+            price: 22,
+            name: "Espresso"
+        },
+        {
+            price: 35,
+            name: "Flat white"
+        },
+        {
+            price: 33,
+            name: "Cappuccino"
+        },
+        {
+            price: 27,
+            name: "Mocha"
+        },
+        {
+            price: 24,
+            name: "Americano"
+        },
+        {
+            price: 26,
+            name: "Macchiato"
+        }
+    ];
+}
+
+function buyItem(name, price) {
+    if (bal >= price) {
+        deductMoney(price);
+        // Todo: Add to list of receipts (DB required)
+        console.log("Purchased: " + name + " for " + price + ",-");
+    } else {
+        console.log("Not enough money");
+    }
+}
+
 function loadReceipts() {
     const receiptList = document.getElementById("receiptList");
     const origReceipt = document.getElementById("originalReceipt");
@@ -50,6 +117,11 @@ function getReceipts() {
 
 function loadBalance() {
     document.getElementById("bal").innerText = bal + ",-";
+}
+
+function deductMoney(value) {
+    bal -= value;
+    loadBalance();
 }
 
 function addMoney() {
